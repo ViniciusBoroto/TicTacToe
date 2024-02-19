70ee1596a65ee199f362833027440b5804e36176
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-02-19 14:21:49 -0300

    turn changes and MakeMove doc

--- Models/Match.cs
+++ Models/Match.cs
@@ -8,12 +8,20 @@ namespace TicTacToe.Models
     public class Match
     {
         private Board _board;
+        private string _player;
         public Board GameBoard { get => _board; }
+        public string player { get => _player; }
         public Match(Board board)
         {
             _board = board;
+            _player = "X";
         }
-        public void MakeMove(Position position, string player)
+        /// <summary>
+        /// Verify if the position is free, makes the move and change the current player
+        /// </summary>
+        /// <param name="position"></param>
+        /// <exception cref="Exception"></exception>
+        public void MakeMove(Position position)
         {
             //Verifica se a posição está vazia
             if (GameBoard.GetBoard()[position.Row, position.Column] != " ")
@@ -21,6 +29,7 @@ namespace TicTacToe.Models
                 throw new Exception("This space has already been selected, choose a free one");
             }
             GameBoard.MakeMove(position, player);
+            _player = _player == "X" ? "Y" : "X";
         }
     }
 }
